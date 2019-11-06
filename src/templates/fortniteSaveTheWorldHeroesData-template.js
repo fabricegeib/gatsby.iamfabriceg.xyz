f1e074a21fdf39ada6c983b8cc8e49e0b05e01b0
Fabrice Geib <fabricegeib@gmail.com>
2019-11-06 15:26:03 +0100

    netlify test

--- src/templates/fortniteSaveTheWorldHeroesData-template.js
+++ src/templates/fortniteSaveTheWorldHeroesData-template.js
@@ -1,26 +1,24 @@
 import React from "react"
 
 // export default ({ pageContext: { heroes } }) => (
-export default ({ pageContext: { heroes } }) =>console.log(heroes) || (
-  <div className="container">
-    <section>
-      <p>{heroes.rarity} | {heroes.type}</p>
-      <p>{heroes.name}</p>
-      <p>{heroes.class}</p>
-      <ul>
-        {/* <li>{heroes.skills[0]}</li> */}
-        {/* <li>{heroes.skills[1]}</li> */}
-        {/* <li>{heroes.skills[2]}</li> */}
-        <li>{heroes.skills.length && heroes.skills[0]}</li>
-        {/* <li>{heroes && heroes.skills[1]}</li> */}
-      </ul>
-    </section>
-
-    <section className="card">
-      <p>{heroes.rarity} | {heroes.type}</p>
-      <p>{heroes.name}</p>
-      <p>{heroes.class}</p>
-      <p>{heroes.perk}</p>
-    </section>
-  </div>
-)
+  export default ({ pageContext: { heroes } }) => heroes !== undefined && (
+    <div className="container">
+      <section>
+        <p>{heroes.rarity} | {heroes.type}</p>
+        <p>{heroes.name}</p>
+        <p>{heroes.class}</p>
+        <ul>
+          {/* <li>{heroes.skills[0]}</li> */}
+          {/* <li>{heroes.skills[1]}</li> */}
+          {/* <li>{heroes.skills[2]}</li> */}
+        </ul>
+      </section>
+  
+      <section className="card">
+        <p>{heroes.rarity} | {heroes.type}</p>
+        <p>{heroes.name}</p>
+        <p>{heroes.class}</p>
+        <p>{heroes.perk}</p>
+      </section>
+    </div>
+  )
