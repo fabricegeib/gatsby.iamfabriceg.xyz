72c7f85844469a748f6769dc98aa8e117f96fab2
Fabrice Geib <fabricegeib@gmail.com>
2019-11-06 23:07:52 +0100

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,43 +6,44 @@
 
 // You can delete this file if you're not using it
 exports.createPages = ({ actions }) => {
-    const { createPage } = actions
-    const gamesData = [
-      {
-        slug: "fortnite",
-        name: "Fortnite",
-        type: "xxx",
-        dateRelease: "2017",
-        "platforms" : [ "Mac", "Mobile", "Playstation", "PC", "Switch", "Xbox" ],
-      },
-      {
-        slug: "red-dead-redemption-2",
-        name: "Red Dead Redemption 2",
-        type: "xxx",
-        dateRelease: "2018",
-        "platforms" : [ "Playstation", "PC", "Xbox" ],
-      },
-    ]
-    gamesData.forEach(games => {
-      createPage({
-        path: `/games/${games.slug}`,
-        component: require.resolve(`./src/templates/gamesData-template.js`),
-        context: { games },
-      })
+  const { createPage } = actions
+  
+  const gamesData = [
+    {
+      slug: "fortnite",
+      name: "Fortnite",
+      type: "xxx",
+      dateRelease: "2017",
+      "platforms" : [ "Android", "iOS", "Mac OS", "Microsoft Windows", "Nintendo Switch", "Playstation 4", "Xbox One" ],
+    },
+    {
+      slug: "red-dead-redemption-2",
+      name: "Red Dead Redemption 2",
+      type: "xxx",
+      dateRelease: "2018",
+      "platforms" : [ "Microsoft Windows", "Playstation 4", "Xbox One" ],
+    },
+  ]
+  gamesData.forEach(games => {
+    createPage({
+      path: `/games/${games.slug}`,
+      component: require.resolve(`./src/templates/gamesData-template.js`),
+      context: { games },
     })
+  })
 
-    const fortniteSaveTheWorldHeroesClassData = [
-      {
-        name: "soldats",
-        title: "Soldats",
-        date: "06/11/2019",
-      },
-    ]
-    fortniteSaveTheWorldHeroesClassData.forEach(heroesClass => {
-      createPage({
-        path: `/fortnite/save-the-world/hereos/${heroesClass.name}`,
-        component: require.resolve(`./src/templates/fortniteSaveTheWorldHeroesClassData-template.js`),
-        context: { heroesClass },
-      })
+  const fortniteSaveTheWorldHeroesClassData = [
+    {
+      name: "soldats",
+      title: "Soldats",
+      date: "06/11/2019",
+    },
+  ]
+  fortniteSaveTheWorldHeroesClassData.forEach(heroesClass => {
+    createPage({
+      path: `/fortnite/save-the-world/hereos/${heroesClass.name}`,
+      component: require.resolve(`./src/templates/fortniteSaveTheWorldHeroesClassData-template.js`),
+      context: { heroesClass },
     })
-  }
+  })
+}
