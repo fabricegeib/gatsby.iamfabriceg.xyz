5fbfc8875a05329da193e3c805f7b26764c836f0
LukyVj <lucas.bonomi@gmail.com>
2019-11-08 19:26:06 +0100

    Update src/pages/fortnite/save-the-worl/heroes/index.js

--- src/pages/fortnite/save-the-world/heroes/index.js
+++ src/pages/fortnite/save-the-world/heroes/index.js
@@ -1,67 +1,69 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby'
-import Image from 'gatsby-image'
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
 
-export const query = graphql `
-    query {
-        allHeroesJson {
-            edges {
-                node {
-                    name
-                    slug
-                    id
-                    class
-                    image {
-                        childImageSharp {
-                            fluid {
-                                ...GatsbyImageSharpFluid
-                            }
-                        }
-                    }
-                }
+export const query = graphql`
+  query {
+    allHeroesJson {
+      edges {
+        node {
+          name
+          slug
+          id
+          class
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
             }
+          }
         }
+      }
     }
-`;
+  }
+`
 
 const HeroesPage = ({ data }) => {
-    // const heroes = data.allHeroesJson;
+  // const heroes = data.allHeroesJson;
 
-    console.log(data)
-    
-    return (
-        <div className="container">
-            <p><Link to="/fortnite/save-the-world">Go back to Save The World</Link></p>
+  console.log(data)
 
-            <h3>Heroes</h3>
+  return (
+    <div className="container">
+      <p>
+        <Link to="/fortnite/save-the-world">Go back to Save The World</Link>
+      </p>
 
-            <div className="cards">
-                {data.allHeroesJson.edges.map(({ node }, index) => (   
-                    <div>
-                        <div className="card" key={index}>
-                            {/* <p className="card__name">{node.name}</p> */}
-                            
-                            <Image
-                                fluid={node.image.childImageSharp.fluid}
-                                alt={node.name}
-                            />
+      <h3>Heroes</h3>
 
-                            <div className="card__overlay">
-                                <p className="card__texte">{node.name}</p>
-                            </div>
-                            <div className="card__bottom">
-                                <p>{node.class}</p>
-                            </div>
-                        </div>
-                        <Link className="link" to={node.slug}>link</Link>
-                    </div>
-                ))}
+      <div className="cards">
+        {data.allHeroesJson.edges.map(({ node }, index) => (
+          <div>
+            <div className="card" key={index}>
+              {node.image && (
+                <Image
+                  fluid={node.image.childImageSharp.fluid}
+                  alt={node.name}
+                />
+              )}
+              <div className="card__overlay">
+                {node.name && <p className="card__texte">{node.name}</p>}
+              </div>
+              <div className="card__bottom">
+                {node.class && <p>{node.class}</p>}
+              </div>
             </div>
+            {node.slug && (
+              <Link className="link" to={node.slug}>
+                link
+              </Link>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-
-            
-        </div>
-    )
-};
-
-export default HeroesPage;
+export default HeroesPage
