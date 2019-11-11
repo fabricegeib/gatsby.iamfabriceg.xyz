a540438f116a7cb296079ae549cdf4181fe53ea4
Fabrice Geib <fabricegeib@gmail.com>
2019-11-11 03:09:19 +0100

    gatsby plugin offline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,13 @@
 
 // You can delete this file if you're not using it
 import "./src/styles/global.scss"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
