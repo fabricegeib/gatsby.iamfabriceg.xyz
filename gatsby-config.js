285adf5ccbfb619c60bc78054c5afb0847509c70
Fabrice Geib <fabricegeib@gmail.com>
2019-12-25 00:06:42 +0100

    remove cypress and change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `iamfabriceg`,
         short_name: `iamfabriceg`,
         start_url: `/`,
-        background_color: `#333`,
-        theme_color: `#333`,
+        background_color: `#000`,
+        theme_color: `#000`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
