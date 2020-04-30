bc7daacc41d8c8d9f825ba1c158c80cd120974d0
Fabrice Geib <fabricegeib@gmail.com>
2020-04-30 14:23:08 +0200

    updates

--- src/pages/red-dead-redemption-2/index.js
+++ src/pages/red-dead-redemption-2/index.js
@@ -4,16 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
-const RedDeadRedemption2Page = () => (
-  <Layout>
-    <SEO title="Red Dead Redemption 2" />
-    <h3><span className="backArrow"><Link to="/">&#8592;</Link></span>Red Dead Redemption 2</h3>
-    <p>Text or description</p>
-    <p>
-        <a href="https://madamnazar.io/" target="_blank" rel="noopener noreferrer">madamnazar.io</a>
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const RedDeadRedemption2Page = class extends React.Component {
+
+  state = {
+    loading: true,
+    error: false,
+    fetchData: []
+  }
+
+  componentDidMount() {
+    fetch('https://madam-nazar-location-api-2.herokuapp.com/location/current').then(response => {
+      console.log(response);
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Red Dead Redemption 2" />
+        <h3><span className="backArrow"><Link to="/">&#8592;</Link></span>Red Dead Redemption 2</h3>
+        <p>Text or description</p>
+        <p>
+            <a href="https://madamnazar.io/" target="_blank" rel="noopener noreferrer">madamnazar.io</a>
+        </p>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default RedDeadRedemption2Page
