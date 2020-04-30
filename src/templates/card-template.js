42ba8344249efe14c5e46867098bbb9f6ff43b5a
Fabrice Geib <fabricegeib@gmail.com>
2020-04-30 15:46:42 +0200

    card template update

--- src/templates/card-template.js
+++ src/templates/card-template.js
@@ -64,36 +64,68 @@ const CardTemplate = (props) => {
                 <Link to="/fortnite/save-the-world/heroes">Fortnite > Save The World > Heroes</Link>
             </p>
 
-            <h3 className="hero__title">{props.data.heroesJson.name}</h3>
-            <h5 className="hero__titleFr">{props.data.heroesJson.nameFr}</h5>
+            <div className="hero_page">
 
-            <div className="hero-container">
-                <div className="heroCard">
-                    <Image
-                        fluid={props.data.heroesJson.image.childImageSharp.fluid}
-                        alt={props.data.heroesJson.name}
-                    />
-                    <div className="heroCard__overlay">
-                        <p>{props.data.heroesJson.name}</p>
-                    </div>
-                    <div className="heroCard__bottom">
-                        <p>{props.data.heroesJson.class}</p>
-                    </div>
-                </div>
-
-                <div className="hero-informations">
-                    <h3 className="hero__class">{props.data.heroesJson.class} | <span>{props.data.heroesJson.rarity}</span></h3>
+                <div className="hero-container">
                     
-                    <h4 className="hero__ability">Abilities</h4>
-                    {props.data && <p>{props.data.heroesJson.abilities[0].title}</p>}
+                    <div className="hero-left">
+                        <h3 className="hero__class"><span>{props.data.heroesJson.rarity} | {props.data.heroesJson.class}</span></h3>
 
-                    <h4 className="hero__ability">Standard Perk</h4>
-                    <p>{props.data.heroesJson.standardPerk}</p>
-                    <h4 className="hero__ability">Commander Perk</h4>
-                    <p>{props.data.heroesJson.commanderPerk}</p>
+                        <h3 className="hero__title">{props.data.heroesJson.name}</h3>
+                        
+                        <div className="heroCard">
+                            <Image
+                                fluid={props.data.heroesJson.image.childImageSharp.fluid}
+                                alt={props.data.heroesJson.name}
+                            />
+                            <div className="heroCard__overlay">
+                                <p>{props.data.heroesJson.name}</p>
+                            </div>
+                            <div className="heroCard__bottom">
+                                <p>{props.data.heroesJson.class}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="hero-informations">
+                        <h4 className="hero__ability">Standard Perk</h4>
+                        <p>{props.data.heroesJson.standardPerk}</p>
+
+                        <h4 className="hero__ability">Commander Perk</h4>
+                        <p>{props.data.heroesJson.commanderPerk}</p>
+
+                        <h4 className="hero__ability">Class Perk</h4>
+                        <p>{props.data.heroesJson.classPerks[0].perk}</p>
+                        <p>{props.data.heroesJson.classPerks[0].text}</p>
+
+                        <p>{props.data.heroesJson.classPerks[1].perk}</p>
+                        <p>{props.data.heroesJson.classPerks[1].text}</p>
 
 
-                    <p>{props.data.heroesJson.citation}</p>
+                        <h4 className="hero__ability">Abilities</h4>
+                        <div>
+                            {props.data && <p>{props.data.heroesJson.abilities[0].title}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[0].cost}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[0].cooldown}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[0].text}</p>}
+                        </div>
+
+                        <div>
+                            {props.data && <p>{props.data.heroesJson.abilities[1].title}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[1].cost}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[1].cooldown}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[1].text}</p>}
+                        </div>
+                        
+                        <div>
+                            {props.data && <p>{props.data.heroesJson.abilities[2].title}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[2].cost}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[2].cooldown}</p>}
+                            {props.data && <p>{props.data.heroesJson.abilities[2].text}</p>}
+                        </div>
+
+                        <p>{props.data.heroesJson.citation}</p>
+                    </div>
                 </div>
             </div>
         </Layout>
