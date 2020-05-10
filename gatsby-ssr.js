979da1bdbd6af4ceb98f32677b4aa6559507e016
Fabrice Geib <fabricegeib@gmail.com>
2020-05-10 14:54:18 +0200

    gatsby test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+const Layout = require("./src/components/layout")
+
+// Adds a class name to the body element
+exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
+  setBodyAttributes({
+    className: "my-body-class",
+  })
+}
+// Wraps every page in a component
+// exports.wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>
+// }
---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+// import React from "react"     
+
+// function CoolComponent({ adjective = 'Cool' }) {
+//   return <p>Youpi So {adjective} !</p>
+// }
+
+// const TestPage = () => (
+//   <div>
+//     <CoolComponent adjective="awesome" />
+//     <CoolComponent />
+//   </div>,
+//   document.getElementById('root')
+// )
+
+// export default TestPage
